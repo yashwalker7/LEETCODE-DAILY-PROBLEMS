bec3214e8504673cdcf32196b72a0394c6f1d39c
yashwalker7 <102256756+yashwalker7@users.noreply.github.com>
2022-09-30 09:04:41 +0530

    Delete 383 - Ransom Note.py

--- 383 - Ransom Note.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        m=list(magazine)
-        for i in ransomNote:
-            if i in m:
-                m.remove(i)
-            else:
-                return False
-        return True
